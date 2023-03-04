873a6f4997adcd076a46b19b508800f14abd614b
Jloon02 <jiunchung2002@gmail.com>
2023-03-04 10:25:03 -0800

    Default config

--- root/src/main/java/com/root_raiser/root/RootApplication.java
+++ root/src/main/java/com/root_raiser/root/RootApplication.java
@@ -2,12 +2,21 @@ package com.root_raiser.root;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class RootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RootApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
 	}
 
 }
